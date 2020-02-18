4c1d1244fd06392ce7522916cd0b995c86f61c52
James Swank <jmswank7@gmail.com>
2020-02-18 11:20:58 -0700

    adds product selector

--- den_temps.py
+++ den_temps.py
@@ -1,5 +1,6 @@
 import dash
 import dash_html_components as html
+import dash_core_components as dcc
 from connect import norm_records, rec_lows, rec_highs, all_temps
 
 app = dash.Dash(__name__)
@@ -23,6 +24,53 @@ def temp_App():
                     className='twelve columns',
                     style={'text-align': 'center'}
                 ),
+            ],
+                className='row'
+            ),
+            html.Div([
+                html.H6(
+                    id='title-date-range',
+                    className='twelve columns',
+                    style={'text-align': 'center'}
+                ),
+            ],
+                className='row'
+            ),
+            html.Div([
+                html.Div([
+                    html.Label('Select Product'),
+                    dcc.RadioItems(
+                        id='product',
+                        options=[
+                            {'label':'Temperature graphs', 'value':'temp-graph'},
+                            {'label':'Climatology for a day', 'value':'climate-for-day'},
+                            {'label':'Full Record Bar Graphs', 'value':'frbg'},
+                            {'label':'5 Year Moving Avgs', 'value':'fyma-graph'},
+                            {'label':'Full Record Heat Map', 'value':'frhm'},
+                        ],
+                        # value='temp-graph',
+                        labelStyle={'display': 'block'},
+                    ),
+                ],
+                    className='three columns',
+                ),
+                # html.Div([
+                #     html.Div(
+                #         id='year-picker'
+                #     ),
+                #     html.Div(
+                #         id='date-picker'
+                #     ),
+                # ],
+                #     className='four columns',
+                # ),
+                # html.Div([
+                #     html.Button('Update Data', id='data-button'),
+                # ]),
+                # html.Div([
+                #     html.Div(id='output-data-button')
+                # ]),
+
             ],
                 className='row'
             ),
