66edaf52599ba2e7f2fe785c6d9bf4766672fa94
James Swank <jmswank7@gmail.com>
2020-03-12 10:14:59 -0600

    cleanup

--- co_2.py
+++ co_2.py
@@ -10,42 +10,6 @@ app = dash.Dash(__name__)
 app.config['suppress_callback_exceptions']=True
 
 
-# old_data = pd.read_csv('ftp://aftp.cmdl.noaa.gov/data/trace_gases/co2/in-situ/surface/mlo/co2_mlo_surface-insitu_1_ccgg_DailyData.txt', delim_whitespace=True, header=[146])
-
-# old_data = old_data.drop(['hour', 'longitude', 'latitude', 'elevation', 'intake_height', 'qcflag', 'nvalue', 'altitude', 'minute', 'second', 'site_code', 'value_std_dev'], axis=1)
-
-# old_data = old_data.iloc[501:]
-
-# old_data.index = pd.to_datetime(old_data[['year', 'month', 'day']])
-# old_data = old_data.drop(['year', 'month', 'day'], axis=1)
-
-# new_data = pd.read_csv('https://www.esrl.noaa.gov/gmd/webdata/ccgg/trends/co2_mlo_weekly.csv')
-
-# new_data['Date'] = pd.to_datetime(new_data['Date'])
-# new_data.index = new_data['Date']
-# new_data = new_data.drop(['month', 'week', 'Date'], axis=1)
-
-# new_data['value'] = new_data['day']
-# new_data = new_data.drop(['day'], axis=1)
-# new_data = new_data[datetime(2019, 1, 1):]
-
-# frames = [old_data, new_data]
-# co2_data = pd.concat(frames)
-# co2_data['value'] = co2_data['value'].replace(-999.99, np.nan)
-# max_co2 = co2_data['value'].max()
-# max_co2_date = co2_data['value'].idxmax().strftime('%Y-%m-%d')
-# current_co2 = co2_data['value'].iloc[-1]
-# current_co2_date = co2_data.index[-1].strftime('%Y-%m-%d')
-
-# fig = go.Figure(data=[go.Scatter(x=co2_data.index, y=co2_data['value'], mode='markers')])
-
-# monthly_avg = new_data.groupby([new_data.index.year, new_data.index.month]).mean()
-# current_year = datetime.now().year
-# current_month = datetime.now().month
-# this_month_avg = monthly_avg.loc[current_year, current_month].value
-# last_year_avg = monthly_avg.loc[current_year-1, current_month].value
-
-
 def co2_App():
     return html.Div(
         [
@@ -96,39 +60,9 @@ def co2_App():
                     className='nine columns'
                 ),
                 html.Div([
-                    # html.Div([
-                    #     html.Div('Maximum CO2 Value (ppm)', style={'text-align':'center'}) 
-                    # ],
-                    #     className='round1'
-                    # ),
-                    # html.Div([
-                    #     html.Div('{}'.format(max_co2), style={'text-align':'center'}),
-                    #     html.Div('{}'.format(max_co2_date), style={'text-align':'center'}) 
-                    # ],
-                    #     className='round1'
-                    # ),
                     html.Div(id='max-co2-layout'),
                     html.Div(id='current-co2-layout'),
                     html.Div(id='avg-co2-layout'),
-                    # html.Div([
-                    #     html.Div('Current CO2 Value (ppm)', style={'text-align':'center'}) 
-                    # ],
-                    #     className='round1'
-                    # ),
-                    # html.Div([
-                    #     html.Div('{}'.format(current_co2), style={'text-align':'center'}),
-                    #     html.Div('{}'.format(current_co2_date), style={'text-align':'center'}) 
-                    # ],
-                    #     className='round1'
-                    # ),
-                    # html.Div([
-                    #     html.Div('Avg For Month (ppm)', style={'text-align':'center'}),
-                    #     html.Div('{:.2f}'.format(this_month_avg), style={'text-align':'center'}), 
-                    #     html.Div('Last Year', style={'text-align':'center'}),
-                    #     html.Div('{:.2f}'.format(last_year_avg), style={'text-align':'center'}), 
-                    # ],
-                    #     className='round1'
-                    # ),
                 ],
                     className='three columns'
                 ),
@@ -143,11 +77,6 @@ def co2_App():
                 ),
             ]),
             html.Div(id='CO2-data', style={'display': 'none'}),
-            # html.Div(id='max-co2', style={'display': 'none'}),
-            # html.Div(id='max-co2-date', style={'display': 'none'}),
-            # html.Div(id='monthly-avg', style={'display': 'none'}),
-            # html.Div(id='this-month-avg', style={'display': 'none'}),
-            # html.Div(id='last-year-avg', style={'display': 'none'}),
         ]
     )
 
