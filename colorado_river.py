e23ea04e9ef2b6eeebb1d198990f7e90e4d6083c
James Swank <jmswank7@gmail.com>
2020-12-03 13:44:54 -0700

    adds calendar year button

--- colorado_river.py
+++ colorado_river.py
@@ -110,6 +110,7 @@ def river_App():
                                 {'label':'W', 'value':'8'},
                                 {'label':'M', 'value':'31'},
                                 {'label':'Y', 'value':'366'},
+                                {'label':'CY', 'value':'CY'},
                             ],
                             value='2',
                             labelStyle={'display': 'inline'},
